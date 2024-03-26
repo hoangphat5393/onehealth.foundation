7ff32687aa2f1b0c51773b5bd8ff73f797a77765
Parker <parker.phat@namtech.sg>
2024-03-26 15:27:42 +0700

    update

--- resources/views/theme/page/bank-online.blade.php
+++ resources/views/theme/page/bank-online.blade.php
@@ -11,17 +11,17 @@
 
         <section class="block17">
             <div class="container">
-                <div class="row my-5">
+                <div class="row my-4">
                     <div class="col">
                         <h2>CÙNG ONEHEALTH FOUNDATION - <br> THẮP SÁNG ƯỚC MƠ CỦA HÀNG TRIỆU GIA ĐÌNH VIỆT NAM</h2>
                     </div>
                 </div>
                 <div class="row mb-5">
-                    <div class="col-md-8">
+                    <div class="col-lg-8 mb-3">
                         {{-- <form name="donateForm" method="post" target="_self" action="https://onehealth.foundation/paypal"> --}}
                         <form name="donateForm" method="post" target="_self" action="#">
 
-                            <!--display error/success message-->
+                            {{-- display error/success message --}}
 
                             <div class="amountDiv mb-3">
                                 <div class="alert alert-success d-none  ">
@@ -36,7 +36,7 @@
                                 <input type="hidden" id="frequency" name="frequency" value="Một lần">
                                 <input type="hidden" name="currency_code" value="USD">
 
-                                <h4 class="tertiary-color text-uppercase">Vui lòng chọn số tiền quyên góp:</h4>
+                                <h4 class="text-main text-uppercase">@lang('Please select the donation amount'):</h4>
                                 <div id="myDIV" class="text-center" style="padding: 20px 0px;height: 100px;">
                                     <a id="btn-10" class="btn btn-amount active" onclick="setDataType(this)" role="button">10 USD</a>
                                     <a id="btn-50" class="btn btn-amount" onclick="setDataType(this)" role="button">50 USD</a>
@@ -60,94 +60,105 @@
                                 </div> --}}
                             </div>
 
-                            <div class="amountDiv donate-payment-account">
-                                <h4 class="tertiary-color text-uppercase mb-3">@lang('Contact information'):</h4>
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating form-floating-sm mb-3">
-                                            <input type="text" name="lastname" class="form-control" id="lastname" placeholder="@lang('Last name')" required>
-                                            <label for="lastname">@lang('Last name')</label>
+                            <div class="wrap-block mb-3">
+                                <div class="p-3">
+                                    <h4 class="bg-main text-uppercase mb-2">@lang('Contact information'):</h4>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="form-floating form-floating-sm mb-3">
+                                                <input type="text" name="lastname" class="form-control" id="lastname" placeholder="@lang('Last name')" required>
+                                                <label for="lastname">@lang('Last name')</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-floating form-floating-sm">
+                                                <input type="text" name="firstname" class="form-control" id="firstname" placeholder="@lang('Last name')" required>
+                                                <label for="firstname">@lang('First name')</label>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-floating form-floating-sm">
-                                            <input type="text" name="firstname" class="form-control" id="firstname" placeholder="@lang('Last name')" required>
-                                            <label for="firstname">@lang('First name')</label>
+
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="form-floating form-floating-sm mb-3 mb-lg-0">
+                                                <input type="email" name="email" class="form-control" id="email" placeholder="@lang('Email')" required>
+                                                <label for="email">@lang('Email address')</label>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-floating form-floating-sm mb-3">
-                                            <input type="email" name="email" class="form-control" id="email" placeholder="@lang('Email')" required>
-                                            <label for="email">@lang('Email address')</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-floating form-floating-sm">
-                                            <input type="text" name="phone" class="form-control" id="phone" placeholder="@lang('Phone')" required>
-                                            <label for="phone">@lang('Phone')</label>
+                                        <div class="col-lg-6">
+                                            <div class="form-floating form-floating-sm mb-3 mb-lg-0">
+                                                <input type="text" name="phone" class="form-control" id="phone" placeholder="@lang('Phone')" required>
+                                                <label for="phone">@lang('Phone')</label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="donationCampaignHeader">
-                                <img src="https://onehealth.foundation/wp-content/themes/thewish/img/icon/icon-ohf-white.png" class="img-fluid">
-                                <span>nếu bạn muốn quyên góp cho các chiến dịch riêng biệt</span>
-                            </div>
+                            <div class="wrap-block">
+                                <div class="donationCampaignHeader d-flex align-items-center p-3">
+                                    <img src="{{ asset('images/icon/icon-ohf-white.png') }}" class="img-fluid me-3" style="width: 40px" />
+                                    <h5>@lang('If you want to donate for separate campaigns')</h5>
+                                </div>
 
-                            {{-- Dự án (Campaign) --}}
-                            @php
-                                $project = \App\Campaign::where('status', 1)->limit(3)->get();
-                            @endphp
-
-                            <div class="amountDiv donationCampaign">
-                                <div id="" class="content clearfix paddingtopmore item-form-container paddingbottom15 uniform-designation">
-                                    <span class="heading">Tôi muốn đóng góp cho chương trình:</span>
-                                    <div class="selector" id="">
-                                        <input type="hidden" name="selectcampagin" id="selectcampagin" value="">
-
-                                        <select class="form-control" id="sel1" onchange="ChangeCampaign(this)">
+                                {{-- Dự án (Campaign) --}}
+                                @php
+                                    $project = \App\Campaign::where('status', 1)->limit(3)->get();
+                                @endphp
+                                <div class="p-3">
+                                    <div class="mb-3">
+                                        <label for="campagin" class="form-label">@lang('I want to contribute to the program'):</label>
+                                        <select name="campagin" class="form-select" id="campagin">
                                             @empty(!$project)
-                                                <option value="">Chọn chiến dịch</option>
+                                                <option value="">@lang('Select campaign')</option>
                                                 @foreach ($project as $item)
                                                     <option value="{{ $item->id }}">{{ $item->name }}</option>
                                                 @endforeach
                                             @endempty
                                         </select>
                                     </div>
-                                    <div class="delicationDiv">
-                                        <span class="heading">Tôi muốn khoản đóng góp của tôi được gửi đến:</span>
-                                        <input type="text" id="delication" name="delication">
+
+                                    <div class="mb-3">
+                                        <label for="delication" class="form-label">@lang('I want my donation sent to'):</label>
+                                        <input type="text" id="delication" name="delication" class="form-control">
                                     </div>
+
+                                    {{-- START MAIL --}}
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="chkPassport">
+                                        <label class="form-check-label" for="chkPassport">
+                                            @lang('Send confirmation to the individual or organization')
+                                        </label>
+                                    </div>
+
+                                    <hr>
+
+                                    <div id="dvPassport" style="display: none">
+                                        <input type="email" id="txtEmail" name="txtEmail" placeholder="@lang('Email')" style="background: #ebebeb;border: 0px;height: 40px;width: 100%;border-radius: 20px;padding: 20px;margin-bottom: 10px;">
+                                        <textarea id="txtMessage" name="txtMessage" rows="5" placeholder="@lang('Message')" style="background: #ebebeb;border: 0px;width: 100%;border-radius: 20px;padding: 20px;margin-bottom: 10px;"></textarea>
+                                    </div>
+                                    {{-- END MAIL --}}
                                 </div>
-                                {{-- START MAIL --}}
-                                <label for="chkPassport">
-                                    <input type="checkbox" id="chkPassport">
-                                    Vui lòng gửi xác nhận cho cá nhân hay tổ chức mà tôi đã quyên góp </label>
-                                <hr>
-                                <div id="dvPassport" style="display: none">
-                                    <input type="email" id="txtEmail" name="txtEmail" placeholder="Email người nhận" style="background: #ebebeb;border: 0px;height: 40px;width: 100%;border-radius: 20px;padding: 20px;margin-bottom: 10px;">
-                                    <textarea rows="5" id="txtMessage" name="txtMessage" placeholder="Nội dung tin nhắn" style="background: #ebebeb;border: 0px;width: 100%;border-radius: 20px;padding: 20px;margin-bottom: 10px;"></textarea>
-                                </div>
-                                {{-- END MAIL --}}
                             </div>
-                            <div class="text-center btn-submit">
-                                <button type="submit" id="submit" class="text-uppercase" onclick="submitTwice(this.form)"><img src="https://onehealth.foundation/wp-content/themes/thewish/img/icon/icon-ohf-white.png">quyên góp ngay </button>
+
+                            <div class="btn-submit mt-3">
+                                <button type="submit" id="submit" class="btn btn-lg d-flex justify-content-center align-items-center" onclick="submitTwice(this.form)">
+                                    <img src="{{ asset('images/icon/icon-ohf-white.png') }}" class="img-fluid me-3" style="width: 40px" />
+                                    @lang('Donate now')
+                                </button>
                             </div>
+
                             <input type="hidden" name="task" value="donate">
+
                         </form>
                     </div>
 
-                    <div class="col-md-4 form-donate-info">
-                        <div>
-                            <div class="text-center" style="margin-bottom: 15px;">
-                                <img class="img-fluid" src="https://onehealth.foundation/wp-content/themes/thewish/img/logo/logo_OHF_color.png">
-                            </div>
-                            <p>Khoản tiền tài trợ của bạn, dù lớn hay nhỏ, đều quan trọng với chúng tôi trên hành trình mang lại nụ cười và tương lai cho trẻ em có hoàn cảnh khó khăn, cải thiện chất lượng các cơ sở y tế và tuổi trẻ Việt Nam. </p>
-                            <img class="img-fluid w-100 mb-3" src="https://onehealth.foundation/wp-content/themes/thewish/img/demo/Img_donate_frm1.jpg">
-                            <img class="img-fluid w-100" src="https://onehealth.foundation/wp-content/themes/thewish/img/demo/Img_donate_frm2.jpg">
+                    <div class="col-lg-4 mb-3 form-donate-info">
+                        <div class="text-center">
+                            <img class="img-fluid" src="{{ setting_option('logo') }}">
                         </div>
+                        <p>Khoản tiền tài trợ của bạn, dù lớn hay nhỏ, đều quan trọng với chúng tôi trên hành trình mang lại nụ cười và tương lai cho trẻ em có hoàn cảnh khó khăn, cải thiện chất lượng các cơ sở y tế và tuổi trẻ Việt Nam. </p>
+                        <img class="img-fluid w-100 mb-3" src="{{ asset('images/donate_frm1.jpg') }}">
+                        <img class="img-fluid w-100" src="{{ asset('images/donate_frm2.jpg') }}">
                     </div>
                 </div>
             </div>
